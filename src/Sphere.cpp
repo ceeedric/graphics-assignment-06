a727613e1fbc2abc90b2c505e2694f5bfd2cfb68
ceeedric <fabiocedric@gmail.com>
2025-04-23 16:12:56 -0500

    Adds more information on Sphere.cpp

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -8,3 +8,9 @@ Sphere::~Sphere()
 {
 }
 
+// an intersect method will be needed here. Spheres in this assignment aren't actual
+// rendered objects they simply return "true" when there is a collision. All
+// we need to do is check against the sphere function
+// The values provided in the assignment PDF should become member variables of Shape
+// except for radius, which will go in Sphere.h.
+
